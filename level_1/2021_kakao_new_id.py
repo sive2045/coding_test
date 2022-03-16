a1c3f3881b2c4daea49e35a22e030b5b4154fc0b
sive2045 <lc9902130509@gmail.com>
2022-03-16 23:00:01 +0900

    feat: 2021 kakao new id

--- level_1/2021_kakao_new_id.py
+++ level_1/2021_kakao_new_id.py
@@ -1,21 +1,34 @@
 """
 신규 아이디 추천
 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/72410
+정규표현식 지식이 필요함
 """
+import re
 
 def solution(new_id):
-    # step 1
-    new_id = new_id.lower()
-    # step 2
-    table = new_id.maketrans({
-        '~': '', '!': '', '@': '', '#': '',
-        '$': '', '%': '', '^': '', '&': '', 
-        '*': '', '(': '', ')': '', '=': '',
-        '+': '', '/': '', '?': '', ',': '',
-        '<': '', '>': '', ':': '', '[': '',
-        ']': '', '}': '', '{': ''
-    })
-    new_id = new_id.translate(table)
+    # step 1 & 2
+    answer = re.sub('[^0-9a-z_.\-]+','',new_id.lower())
+
     # step 3
+    answer = re.sub('\.*\.+','.', answer)
     
-    return 0
+    # step 4
+    answer = answer.strip('.')
+
+    # step 5
+    if answer == '':
+        answer = 'a'
+
+    # step 6
+    answer = answer[:15].rstrip('.')
+
+    # step 7
+    answer += answer[-1] * (3-len(answer))
+
+    return answer
+
+################################################################################
+new_id = "...!@BaT#*..y.abcdefghijklm"	
+################################################################################
+
+print(solution(new_id))
